c6c979df2c251495794d2110ee93ae8db75b1a3c
cgdppl <dominikpotoczniak@gmail.com>
2025-12-30 16:28:11 +0100

    Comments added for clarity

--- src/com/walk/Main.kt
+++ src/com/walk/Main.kt
@@ -1,27 +1,32 @@
 //main function of the project
 fun main(args: Array<String>){
     //create all class objects
-    var myWalk = WalkSimualtion();
-    var myCommunicator = FileCommunicator();
+    var myWalk = WalkSimualtion();      //used for making the walk
+    var myCommunicator = FileCommunicator();        //used for writing and reading from files
     
-    //declaring vars for results and operations
-    var inputArray : ArrayList<String> = arrayListOf<String>();
-    var walkValueArray : ArrayList<Int> = arrayListOf<Int>();
-    var walkPathArray : ArrayList<String> = arrayListOf<String>();
-    var chanceUp : Double? = null;
-    var numberOfSteps : Int? = null;
-    var inputFile : String = "InputData.txt";
-    var outputFile : String = "OutputFile.txt" 
+    //array lists for storing values 
+    var inputArray : ArrayList<String> = arrayListOf<String>();     //array of inputs for walk
+    var walkValueArray : ArrayList<Int> = arrayListOf<Int>();       //array of walk values
+    var walkPathArray : ArrayList<String> = arrayListOf<String>();      //array of walk paths
+    //neeed vars
+    var chanceUp : Double? = null;      //var for storing chance of walk going up
+    var numberOfSteps : Int? = null;        //var for storing number of steps walk has to take
+    var inputFile : String = "InputData.txt";       //input file
+    var outputFile : String = "OutputFile.txt"      //output file
     
+    //read all inputs from file
     inputArray = myCommunicator.read(inputFile);
+    //read all inputs into vars
     for(input in inputArray){
-        var inputsSplit = input.split(";")
-        chanceUp = inputsSplit[0].toDouble();
-        numberOfSteps = inputsSplit[1].toInt();
+        var inputsSplit = input.split(";")      //split inputs at ";"
+        chanceUp = inputsSplit[0].toDouble();       //first part is the chance of going up
+        numberOfSteps = inputsSplit[1].toInt();     //second is the number of steps to be taken
+        //perform the walk
         var (walkValue, walkPath) = myWalk.walk(chanceUp!!, numberOfSteps!!);
-        walkValueArray.add(walkValue);
-        walkPathArray.add(walkPath);
+        walkValueArray.add(walkValue);      //add walk value to the array 
+        walkPathArray.add(walkPath);        //ad walk path to the array
     }
+    //write the results to the output file
     myCommunicator.write(walkValueArray, walkPathArray, walkValueArray.size, outputFile);
 
 }
